02a001a71a8d0d201bfc828d8e05fec4b833342c
Pasha127 <pasha.account@gmail.com>
2022-08-25 16:50:18 +0200

    Update index.js

--- index.js
+++ index.js
@@ -8,3 +8,25 @@ headers: {
 window.onload = () => {
     getProducts();
 }
+
+//product model full
+//{
+//    "_id": "5d318e1a8541744830bef139", //SERVER GENERATED
+//    "name": "app test 1",  //REQUIRED
+//    "description": "somthing longer", //REQUIRED
+//    "brand": "nokia", //REQUIRED
+//    "imageUrl": "https://drop.ndtv.com/TECH/product_database/images/2152017124957PM_635_nokia_3310.jpeg?downsize=*:420&output-quality=80", //REQUIRED
+//    "price": 100, //REQUIRED
+//    "userId": "admin", //SERVER GENERATED
+//    "createdAt": "2019-07-19T09:32:10.535Z", //SERVER GENERATED
+//    "updatedAt": "2019-07-19T09:32:10.535Z", //SERVER GENERATED
+//    "__v": 0 //SERVER GENERATED
+//  }
+//product model user
+//{    
+//    "name": "app test 1",  //REQUIRED
+//    "description": "somthing longer", //REQUIRED
+//    "brand": "nokia", //REQUIRED
+//    "imageUrl": "https://drop.ndtv.com/TECH/product_database/images/2152017124957PM_635_nokia_3310.jpeg?downsize=*:420&output-quality=80", //REQUIRED
+//    "price": 100, //REQUIRED
+//  }
