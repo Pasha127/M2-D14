06d690640322968a6f763d93633cb5622b52e3a3
Pasha127 <pasha.account@gmail.com>
2022-08-25 23:16:10 +0200

    multiple pages added

--- back-office.js
+++ back-office.js
@@ -11,26 +11,25 @@ const viewCartButton = document.querySelector("#viewCartButton");
 const clearCartButton = document.querySelector("#clearCartButton");
 const header = document.querySelector("nav");
 const logInBtn = document.querySelector("#logInButton");
-let query = searchField.value;
 let searchResultArr = []; 
 let cartArr = [];
 let catalog = {};
 let data = [];
 
 
-//const  loadDefaultProducts = async () => {
-//    try{        
-//        const response = 
-//        await fetch("https://striveschool-api.herokuapp.com/api/product/", {
-//            headers: {"Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MzA3ODg0NDFlYjc2ZDAwMTUxNTAxZjgiLCJpYXQiOjE2NjE0MzgwMjAsImV4cCI6MTY2MjY0NzYyMH0.FxXNN1ADQHPQJbchifn_vi_cp1sdPcdONESnfaMV4DE"}});
-//            data = await response.json();
-//            console.log("data:",data);
-//            return data;
-//        }
-//        catch(err){console.log(err)};
-//        
-//        
-//    }
+const  loadDefaultProducts = async () => {
+    try{        
+        const response = 
+        await fetch("https://striveschool-api.herokuapp.com/api/product/", {
+            headers: {"Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MzA3ODg0NDFlYjc2ZDAwMTUxNTAxZjgiLCJpYXQiOjE2NjE0MzgwMjAsImV4cCI6MTY2MjY0NzYyMH0.FxXNN1ADQHPQJbchifn_vi_cp1sdPcdONESnfaMV4DE"}});
+            data = await response.json();
+            console.log("data:",data);
+            return data;
+        }
+        catch(err){console.log(err)};
+        
+        
+    }
     //const  loadDefaultProducts = async () => {
     //    try{
     //        const response = await fetch("https://striveschool-api.herokuapp.com/books");
@@ -41,22 +40,22 @@ let data = [];
     //    catch(err){console.log(err)};
     //    console.log(catalog);
     //};
-//    const searchCatalog = () => {
-//        searchResultArr = [];
-//        console.log(query);
-//        modal.querySelector("div.modal-header").innerText  = `Search: "${query}"`;
-//        //modal.querySelector(".modal-body img").setAttribute("src", catalog[0].img) ;
-//        for(let i=0; i<catalog.length; i++){
-//            //console.log(catalog[i].title.toLowerCase().includes(query.toLowerCase()));
-//            if(catalog[i].title.toLowerCase().includes(query.toLowerCase())){
-//                searchResultArr.push(catalog[i]);
-//                
-//            }
-//        }
-//        console.log(searchResultArr);
-//        makeCards(modalBody, searchResultArr);    
-//        
-//    }
+    const searchCatalog = () => {
+        searchResultArr = [];
+        console.log(query);
+        modal.querySelector("div.modal-header").innerText  = `Search: "${query}"`;
+        //modal.querySelector(".modal-body img").setAttribute("src", catalog[0].img) ;
+        for(let i=0; i<catalog.length; i++){
+            //console.log(catalog[i].title.toLowerCase().includes(query.toLowerCase()));
+            if(catalog[i].title.toLowerCase().includes(query.toLowerCase())){
+                searchResultArr.push(catalog[i]);
+                
+            }
+        }
+        console.log(searchResultArr);
+        makeCards(modalBody, searchResultArr);    
+        
+    }
     
     
     const changeDisplayed = () => {
@@ -85,45 +84,42 @@ let data = [];
         };
         makeCards(cardContainerRow, catalog);
     }
-    //const showCart = () => {
-    //    if(cartArr.length === 1){
-    //        modal.querySelector("div.modal-header").innerText  = `Your Cart has 1 item.`;
-    //    }else{    
-    //        modal.querySelector("div.modal-header").innerText  = `Your Cart has ${cartArr.length} items.`;
-    //    }   
-    //
-    //    
-    //    for(card of modalBody.querySelectorAll("*")){
-    //        card.remove();
-    //    }
-    //    cartArr.forEach(element => {
-    //        const newP = document.createElement('p') 
-    //        newP.innerText = element;
-    //        modalBody.append(newP);
-    //    })   
-    //}
+    const showCart = () => {
+        if(cartArr.length === 1){
+            modal.querySelector("div.modal-header").innerText  = `Your Cart has 1 item.`;
+        }else{    
+            modal.querySelector("div.modal-header").innerText  = `Your Cart has ${cartArr.length} items.`;
+        }   
     
-    //const addToList = (e) => {    
-    //    const addedProduct = catalog.filter(element => element.title === e.target.closest(".card").querySelector(".card-text").innerText);
-    //    cartArr.push(addedProduct[0].title);
-    //    e.target.closest(".card").classList.add("selcted4Cart");
-    //
-    //    
-    //}
-    const addToList = (e) => {
+        
+        for(card of modalBody.querySelectorAll("*")){
+            card.remove();
+        }
+        cartArr.forEach(element => {
+            const newP = document.createElement('p') 
+            newP.innerText = element;
+            modalBody.append(newP);
+        })   
+    }
+    
+    const addToCart = (e) => {    
+        const addedProduct = catalog.filter(element => element.title === e.target.closest(".card").querySelector(".card-text").innerText);
+        cartArr.push(addedProduct[0].title);
+        e.target.closest(".card").classList.add("selcted4Cart");
+    
         
     }
-    //const clearCart = () =>{
-    //    resetBoard();
-    //    cartArr=[];
-    //   
-    //    modal.querySelector("div.modal-header").innerText  = `Your Cart has ${cartArr.length} items.`;
-    //
-    //    for(card of modalBody.querySelectorAll("*")){
-    //        card.remove();
-    //    }
-    //}
-    const deleteCard = (e) => {
+    const clearCart = () =>{
+        resetBoard();
+        cartArr=[];
+       
+        modal.querySelector("div.modal-header").innerText  = `Your Cart has ${cartArr.length} items.`;
+    
+        for(card of modalBody.querySelectorAll("*")){
+            card.remove();
+        }
+    }
+    const skip = (e) => {
         e.target.closest("div.col-md-4").remove();
     }
     
@@ -165,8 +161,8 @@ let data = [];
         });
         const cartBtns = document.querySelectorAll(".addCart");
         const skipBtns = document.querySelectorAll(".skipBtn");
-        cartBtns.forEach(btn => btn.addEventListener("click", addToList));
-        skipBtns.forEach(btn => btn.addEventListener("click", deleteCard));
+        cartBtns.forEach(btn => btn.addEventListener("click", addToCart));
+        skipBtns.forEach(btn => btn.addEventListener("click", skip));
     }
     const enroll = () => {
         if(userInput.value === "admin" && inputPassword.value === "password"){
@@ -196,10 +192,10 @@ const clearInputFields = () => {
 
 window.onload = () => {
     
-    
-    
+    loadDefaultProducts();    
+    searchField.addEventListener("keydown", changeDisplayed)
     searchButton.addEventListener("click", resetBoard);
-
+    viewCartButton.addEventListener("click", showCart);
     clearCartButton.addEventListener("click", clearCart);
     enrollBtn.addEventListener("click", enroll);
     logInBtn.addEventListener("click", clearInputFields);
