024c28adb071bb72e658a9a7717b2ea27a8330b5
Pasha127 <pasha.account@gmail.com>
2022-08-25 16:57:57 +0200

    fetch problem trying to debug

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
+let obj = {};
 const getProducts = async () => {
     fetch("https://striveschool-api.herokuapp.com/api/put-your-endpoint-here/", {
 headers: {
 "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MzA3ODg0NDFlYjc2ZDAwMTUxNTAxZjgiLCJpYXQiOjE2NjE0MzgwMjAsImV4cCI6MTY2MjY0NzYyMH0.FxXNN1ADQHPQJbchifn_vi_cp1sdPcdONESnfaMV4DE"
 }
-}).then(response => console.log(response))
+}).then(response => obj = response).then(console.log(obj)).catch(err => console.log(err));
+
 }
 window.onload = () => {
     getProducts();
