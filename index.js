ae2eb030bb92212712cdfc1f02e250c739027c24
Pasha127 <pasha.account@gmail.com>
2022-08-25 16:42:09 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const getProducts = async () => {
+    fetch("https://striveschool-api.herokuapp.com/api/put-your-endpoint-here/", {
+headers: {
+"Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MzA3ODg0NDFlYjc2ZDAwMTUxNTAxZjgiLCJpYXQiOjE2NjE0MzgwMjAsImV4cCI6MTY2MjY0NzYyMH0.FxXNN1ADQHPQJbchifn_vi_cp1sdPcdONESnfaMV4DE"
+}
+}).then(response => )
+}
+window.onload = () => {
+    getProducts();
+}
